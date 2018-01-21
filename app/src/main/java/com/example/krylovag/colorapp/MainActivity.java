d3d7d5746c52507bf6d5f934b4329f218542007d
AlexKrylov <33526162+AlexKrylov@users.noreply.github.com>
2018-01-21 23:43:55 +0300

    Added share button.

--- app/src/main/java/com/example/krylovag/colorapp/MainActivity.java
+++ app/src/main/java/com/example/krylovag/colorapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.krylovag.colorapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,11 +13,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Spinner spinner;
+    private Intent intent;
     public static final String APP_PREFERENCES = "mysettings";
     public static final String APP_PREFERENCES_COLOR = "Color";
     private SharedPreferences mSettings;
     private SharedPreferences.Editor prefEditor;
-    int c;
+    int saved_color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +28,16 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textview_description);
         Button button = (Button) findViewById(R.id.button_show_description);
         button.setOnClickListener(onClickListener);
+        Button send = findViewById(R.id.share_button);
+        send.setOnClickListener(shareWithAFriend);
 
         spinner = (Spinner) findViewById(R.id.spinner_for_color);
         if (mSettings.contains(APP_PREFERENCES_COLOR)) {
-            c = mSettings.getInt(APP_PREFERENCES_COLOR, 0);
+            saved_color = mSettings.getInt(APP_PREFERENCES_COLOR, 0);
         } else {
-            c = 0;
+            saved_color = 0;
         }
-        spinner.setSelection(c);
+        spinner.setSelection(saved_color);
     }
 
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -42,21 +46,44 @@ public class MainActivity extends AppCompatActivity {
             if (view.getId() == R.id.button_show_description) {
                 String result = ColorSpec.getEffect(MainActivity.this, spinner.getSelectedItemPosition());
                 textView.setText(result);
-                if (spinner.getSelectedItemPosition() == 0) {
-                    textView.setBackgroundResource(R.color.red);
-                } else if (spinner.getSelectedItemPosition() == 1) {
-                    textView.setBackgroundResource(R.color.orange);
-                } else if (spinner.getSelectedItemPosition() == 2) {
-                    textView.setBackgroundResource(R.color.yellow);
-                } else if (spinner.getSelectedItemPosition() == 3) {
-                    textView.setBackgroundResource(R.color.green);
-                } else if (spinner.getSelectedItemPosition() == 4) {
-                    textView.setBackgroundResource(R.color.light_blue);
-                } else if (spinner.getSelectedItemPosition() == 5) {
-                    textView.setBackgroundResource(R.color.blue);
-                } else if (spinner.getSelectedItemPosition() == 6) {
-                    textView.setBackgroundResource(R.color.purple);
+                switch (spinner.getSelectedItemPosition()) {
+                    case 0:
+                        textView.setBackgroundResource(R.color.red);
+                        break;
+                    case 1:
+                        textView.setBackgroundResource(R.color.orange);
+                        break;
+                    case 2:
+                        textView.setBackgroundResource(R.color.yellow);
+                        break;
+                    case 3:
+                        textView.setBackgroundResource(R.color.green);
+                        break;
+                    case 4:
+                        textView.setBackgroundResource(R.color.light_blue);
+                        break;
+                    case 5:
+                        textView.setBackgroundResource(R.color.blue);
+                        break;
+                    case 6:
+                        textView.setBackgroundResource(R.color.purple);
+                        break;
                 }
+            }
+        }
+    };
+
+    private final View.OnClickListener shareWithAFriend = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.share_button) {
+                String messageText = ColorSpec.getEffect(MainActivity.this, spinner.getSelectedItemPosition());
+                intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, messageText);
+                String chooserTitle = getString(R.string.choose_title);
+                Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+                startActivity(chosenIntent);
             }
         }
     };
