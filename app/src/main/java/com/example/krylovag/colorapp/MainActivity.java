d2186244793eca4d1c1a04d6084a526d227099e9
AlexKrylov <33526162+AlexKrylov@users.noreply.github.com>
2018-01-25 00:19:24 +0300

    Added onSaveInstanceState()

--- app/src/main/java/com/example/krylovag/colorapp/MainActivity.java
+++ app/src/main/java/com/example/krylovag/colorapp/MainActivity.java
@@ -6,12 +6,14 @@ import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "";
     private TextView textView;
     private Spinner spinner;
     private Intent intent;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences.Editor prefEditor;
     int saved_description;
     String saved_text;
+    int saved_position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +39,21 @@ public class MainActivity extends AppCompatActivity {
 
         spinner = findViewById(R.id.spinner_for_color);
         if (mSettings.contains(APP_PREFERENCES_POSITION)) {
-            saved_description = mSettings.getInt(APP_PREFERENCES_POSITION, 0);
+            saved_position = mSettings.getInt(APP_PREFERENCES_POSITION, 0);
         } else {
-            saved_description = 0;
+            saved_position = 0;
         }
-        spinner.setSelection(saved_description);
+        spinner.setSelection(saved_position);
 
         if(mSettings.contains(APP_PREFERENCES_DESCRIPTION)) {
             saved_text = mSettings.getString(APP_PREFERENCES_DESCRIPTION, null);
         }
         textView.setText(saved_text);
+
+        Log.i(TAG, "savedInstanceState " + savedInstanceState);
+        if (savedInstanceState != null) {
+            saved_position = savedInstanceState.getInt(APP_PREFERENCES_POSITION);
+        }
      }
 
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -105,5 +113,12 @@ public class MainActivity extends AppCompatActivity {
         String save_desc = ColorSpec.getEffect(MainActivity.this, spinner.getSelectedItemPosition());
         prefEditor.putString(APP_PREFERENCES_DESCRIPTION, save_desc);
         prefEditor.apply();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(APP_PREFERENCES_POSITION, saved_position);
     }
 }
