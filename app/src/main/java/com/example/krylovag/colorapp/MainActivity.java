0f8cd46a02a8a9eabe0aefe380057d289f4fcdcd
AlexKrylov <33526162+AlexKrylov@users.noreply.github.com>
2018-01-24 23:54:28 +0300

    Added save color description.

--- app/src/main/java/com/example/krylovag/colorapp/MainActivity.java
+++ app/src/main/java/com/example/krylovag/colorapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.krylovag.colorapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,30 +16,37 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spinner;
     private Intent intent;
     public static final String APP_PREFERENCES = "mysettings";
-    public static final String APP_PREFERENCES_COLOR = "Color";
+    public static final String APP_PREFERENCES_POSITION = "Position";
+    public static final String APP_PREFERENCES_DESCRIPTION = "Description";
     private SharedPreferences mSettings;
     private SharedPreferences.Editor prefEditor;
-    int saved_color;
+    int saved_description;
+    String saved_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
-        textView = (TextView) findViewById(R.id.textview_description);
-        Button button = (Button) findViewById(R.id.button_show_description);
+        textView = findViewById(R.id.textview_description);
+        Button button = findViewById(R.id.button_show_description);
         button.setOnClickListener(onClickListener);
         Button send = findViewById(R.id.share_button);
         send.setOnClickListener(shareWithAFriend);
 
-        spinner = (Spinner) findViewById(R.id.spinner_for_color);
-        if (mSettings.contains(APP_PREFERENCES_COLOR)) {
-            saved_color = mSettings.getInt(APP_PREFERENCES_COLOR, 0);
+        spinner = findViewById(R.id.spinner_for_color);
+        if (mSettings.contains(APP_PREFERENCES_POSITION)) {
+            saved_description = mSettings.getInt(APP_PREFERENCES_POSITION, 0);
         } else {
-            saved_color = 0;
+            saved_description = 0;
         }
-        spinner.setSelection(saved_color);
-    }
+        spinner.setSelection(saved_description);
+
+        if(mSettings.contains(APP_PREFERENCES_DESCRIPTION)) {
+            saved_text = mSettings.getString(APP_PREFERENCES_DESCRIPTION, null);
+        }
+        textView.setText(saved_text);
+     }
 
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
@@ -91,9 +99,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        int save_color = spinner.getSelectedItemPosition();
+        int save_description = spinner.getSelectedItemPosition();
         prefEditor = mSettings.edit();
-        prefEditor.putInt(APP_PREFERENCES_COLOR, save_color);
+        prefEditor.putInt(APP_PREFERENCES_POSITION, save_description);
+        String save_desc = ColorSpec.getEffect(MainActivity.this, spinner.getSelectedItemPosition());
+        prefEditor.putString(APP_PREFERENCES_DESCRIPTION, save_desc);
         prefEditor.apply();
     }
 }
